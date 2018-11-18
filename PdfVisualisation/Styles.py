d2e0612301dadd9017bd88e0d0a693c9f105ada3
Bjoern Ludwig <bjoern.ludwig@b-ludwig.de>
2018-11-18 02:20:00 +0100

    Remove unneeded parenthesis in Styles.py returns

--- PdfVisualisation/Styles.py
+++ PdfVisualisation/Styles.py
@@ -6,16 +6,16 @@ class Styles:
 
     @staticmethod
     def inner_grid(colors):
-        return ('INNERGRID', (0, 0), (-1, -1), 0.25, colors)
+        return 'INNERGRID', (0, 0), (-1, -1), 0.25, colors
 
     @staticmethod
     def box(colors):
-        return ('BOX', (0, 0), (-1, -1), 0.25, colors)
+        return 'BOX', (0, 0), (-1, -1), 0.25, colors
 
     @staticmethod
     def background(colors):
-        return ('BACKGROUND', (0, 0), (-1, -1), colors)
+        return 'BACKGROUND', (0, 0), (-1, -1), colors
 
     @staticmethod
     def font_size(size):
-        return ('FONTSIZE', (0, 0), (-1, -1), size)
+        return 'FONTSIZE', (0, 0), (-1, -1), size
