c1f971f34b2225bcececb97fb6bfbfdd4813a125
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-10-14 18:16:46 +0200

    Issue black on Sorter.py

--- Core/Sorter.py
+++ Core/Sorter.py
@@ -12,9 +12,9 @@ class Sorter:
         def get_key(course):
             _key = course.findtext(sort_key)
             if _key:
-                return datetime.strptime(_key, '%d.%m.%Y %H:%M')
+                return datetime.strptime(_key, "%d.%m.%Y %H:%M")
             else:
-                return datetime.strptime('01.01.2099 00:00', '%d.%m.%Y %H:%M')
+                return datetime.strptime("01.01.2099 00:00", "%d.%m.%Y %H:%M")
 
         self._courses[:] = sorted(self._courses, key=get_key)
         return self._courses[:]
