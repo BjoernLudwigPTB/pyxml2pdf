f1f5ad26677d01f03ce2d7ceb57c5e4cf468564c
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-10-14 18:10:10 +0200

    Issue black on Downloader.py

--- Core/Downloader.py
+++ Core/Downloader.py
@@ -1,11 +1,10 @@
 import requests
 
 
-class Downloader():
-
+class Downloader:
     def __init__(self, url):
         self.__url = url
 
     def download(self, output_filename):
         answer = requests.get(self.__url, allow_redirects=False)
-        open(output_filename, 'wb').write(answer.content)
+        open(output_filename, "wb").write(answer.content)
