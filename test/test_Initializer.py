f47caf2d14d6a2f8b768df374a6b96af7340bf35
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-10-14 16:18:57 +0200

    Rename test variable to suffice PEP 8

--- test/test_Initializer.py
+++ test/test_Initializer.py
@@ -6,8 +6,8 @@ from Core.Initializer import Initializer
 @pytest.fixture
 def download():
     from Core.Downloader import Downloader
-    dl = Downloader('https://www.alpinclub-berlin.de/kv/kursdaten.xml')
-    dl.download('input/kursdaten.xml')
+    downloader = Downloader('https://www.alpinclub-berlin.de/kv/kursdaten.xml')
+    downloader.download('input/kursdaten.xml')
 
 
 def test_initializer_init():
