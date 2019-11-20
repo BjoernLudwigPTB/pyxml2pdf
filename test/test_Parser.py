7e4d3af6c5e7d60d752aadb339bbe5e3f7dd71cc
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-11-20 11:09:13 +0100

    Include module items with class Item

--- test/test_Parser.py
+++ test/test_Parser.py
@@ -7,9 +7,3 @@ def test_collect_xml_data_empty_call():
     parser = Parser("test")
     with pytest.warns(RuntimeWarning):
         parser.collect_xml_data(events=None)
-
-
-def test_collect_event_data_empty_call():
-    parser = Parser("test")
-    with pytest.warns(RuntimeWarning):
-        parser.collect_event_data(event=None)
