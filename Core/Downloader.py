d0c9c674c7a961409341507df35ce7f11e0e26fd
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-11-21 22:48:27 +0100

    Resolve Codacy issues in Downloader

--- Core/Downloader.py
+++ Core/Downloader.py
@@ -4,8 +4,8 @@ from clint.textui import progress
 
 class Downloader:
 
-    __url: str
-    __output_filename: str
+    _url: str
+    _output_filename: str
 
     def __init__(self, url, output_filename):
         """
@@ -15,26 +15,26 @@ class Downloader:
         :param str output_filename: the local path where and under what name to store
             the downloaded file
         """
-        self.__url = url
-        self.__output_filename = output_filename
+        self._url = url
+        self._output_filename = output_filename
         open(output_filename, "wb").write(
             requests.get(url, allow_redirects=False).content
         )
 
         local_filename = self.extract_filename_from_url()
-        r = requests.get(url, stream=True)
+        response = requests.get(url, stream=True)
 
         # Compute parameters for download and corresponding progress bar.
-        total_length = int(r.headers.get("content-length"))
+        total_length = int(response.headers.get("content-length"))
         chunk_size = 512 * 1024
 
-        f = open(local_filename, "wb")
+        file = open(local_filename, "wb")
         for chunk in progress.bar(
-            r.iter_content(chunk_size), expected_size=(total_length / 1024) + 1
+            response.iter_content(chunk_size), expected_size=(total_length / 1024) + 1
         ):
             if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-        f.close()
+                file.write(chunk)
+        file.close()
 
     def extract_filename_from_url(self):
-        return self.__url.split("/")[-1]
+        return self._url.split("/")[-1]
