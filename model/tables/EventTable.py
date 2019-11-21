f272b9eb926842fa5973815e941870df714e7059
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-11-21 20:46:14 +0100

    Refactor EventTable
    Enhance comments, type hints and replace all occurrences of item with
    event.

--- model/tables/EventTable.py
+++ model/tables/EventTable.py
@@ -1,14 +1,22 @@
 from typing import List
 
+from reportlab.platypus import Table
+
 
 class EventTable:
-    def __init__(self, name, locations, activities):
+
+    _title: str
+    _locations: List[str]
+    _activities: List[str]
+    _elements: List[Table]
+
+    def __init__(self, title, locations, activities):
         """
         Contains all events which cover the mentioned activities at the
-        mentioned locations. Every item listed covers at least one of the
+        mentioned locations. Every event listed covers at least one of the
         listed activities at one of the listed locations.
 
-        :param str name: name of the table describing where and what is done in
+        :param str title: name of the table describing where and what is done in
             the included events
         :param List[str] locations: list of locations where the listed activities take
             place
@@ -16,40 +24,34 @@ class EventTable:
             events
         """
         self._elements = []
-        self._styles = []
-        self._name = name
+        self._title = title
         self._locations = locations
         self._activities = activities
 
-    def add_event(self, event):
-        """
-        Adds a single item to the table later to be displayed as part of the
-        table.
+    def append(self, event):
+        """Append an event to the end of the table
 
-        :param List[reportlab.platypus.Table] event:
+        :param reportlab.platypus.Table event:
         """
         self._elements.append(event)
 
     def get_elements(self):
-        """
-        Return the collected item table.
+        """Return the whole item table
 
         :return list[reportlab.platypus.Table]: a list of all table rows
-            containing the relevant item data
+            containing the relevant event data
         """
         return self._elements
 
     def get_activities(self):
-        """
-        Return the activities associated with the item table.
+        """Return the activities shown in the event table
 
         :return list[str]: a list of all activities
         """
         return self._activities
 
     def get_locations(self):
-        """
-        Return the locations on which the events of this table take place.
+        """Return the locations at which the events of this table take place
 
         :return list[str]: a list of all locations
         """
