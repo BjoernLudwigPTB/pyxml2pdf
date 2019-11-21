ca1535cc25b33d163ad6e04cb40079856e34f163
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-11-21 20:52:56 +0100

    Replace item by event in Creator

--- model/tables/Creator.py
+++ model/tables/Creator.py
@@ -1,21 +1,25 @@
+from typing import List, Tuple, Union
+
 from reportlab.platypus import Table
 
 
 class Creator:
     @staticmethod
     def create_fixedwidth_table(elements, widths, style):
-        """ Create and return a table with specified column widths
+        """Create a table with specified column widths
 
         Create a table from specified elements with fixed column widths and a specific
         style.
 
-        :param  elements: the elements to include in the table
-        :param widths: the column widths
-        :param style: the style in which the table shall appear
-        :returns: table containing all specified elements in fixed width columns
-        :rtype: Table
+        :param List[List[reportlab.platypus.Flowable]] elements: the cell values
+            wrapped by a List representing the columns wrapped by a list representing
+            the lines of the table
+        :param Union[float, List[float]] widths: the column widths
+        :param List[Tuple[Union[str, Tuple[int]]]] style: the style in which the table
+            shall appear
+        :returns Table: table containing all specified elements in fixed width columns
         """
-        t = Table(elements, colWidths=widths)
-        t.setStyle(style)
+        table = Table(elements, colWidths=widths)
+        table.setStyle(style)
 
-        return t
+        return table
