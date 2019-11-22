462822b089c8da02bf963eaf7e163efb5521b4c8
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-11-22 23:05:19 +0100

    Repair docstring for Initializer

--- Core/Initializer.py
+++ Core/Initializer.py
@@ -11,18 +11,16 @@ from Core.Sorter import Sorter
 
 
 class Initializer:
+    """Coordinate the construction of the pdf result
+
+    :param str input_path: path to input xml-file
+    :param str output_path: path to pdf file containing result
+    :param str properties_path: path to text file containing properties
+    """
 
     __data: List[KeepTogether]
 
     def __init__(self, input_path, output_path, properties_path):
-        """Coordinate the construction of the pdf result
-
-        Parameters
-        ----------
-        :param str input_path: path to input xml-file
-        :param str output_path: path to pdf file containing result
-        :param str properties_path: path to text file containing properties
-        """
         self.__data = []
         parser = Parser(properties_path, self.__data)
         pdf = SimpleDocTemplate(
