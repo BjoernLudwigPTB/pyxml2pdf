8cc22d34fe0b24af8ff2db1380d7f920777285e4
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-11-23 12:49:47 +0100

    Enhance EventTable docstring and refactor getters

--- model/tables/EventTable.py
+++ model/tables/EventTable.py
@@ -4,26 +4,25 @@ from reportlab.platypus import Table
 
 
 class EventTable:
+    """An EventTable contains a subset of the xml inputs
+
+    Contains all events which cover the mentioned activities at the
+    mentioned locations. Every event listed covers at least one of the
+    listed activities at one of the listed locations.
+
+    :param str title: name of the table describing where and what is done in
+        the included events
+    :param List[str] locations: list of locations where the activities take place
+    :param List[str] activities: list of activities covered by the listed events
+    """
 
     _title: str
     _locations: List[str]
     _activities: List[str]
-    _elements: List[Table]
+    _events: List[Table]
 
     def __init__(self, title, locations, activities):
-        """
-        Contains all events which cover the mentioned activities at the
-        mentioned locations. Every event listed covers at least one of the
-        listed activities at one of the listed locations.
-
-        :param str title: name of the table describing where and what is done in
-            the included events
-        :param List[str] locations: list of locations where the listed activities take
-            place
-        :param List[str] activities: list of activities covered by the listed
-            events
-        """
-        self._elements = []
+        self._events = []
         self._title = title
         self._locations = locations
         self._activities = activities
@@ -33,26 +32,33 @@ class EventTable:
 
         :param reportlab.platypus.Table event:
         """
-        self._elements.append(event)
+        self._events.append(event)
 
-    def get_elements(self):
+    def events(self):
         """Return the whole item table
 
-        :return list[reportlab.platypus.Table]: a list of all table rows
+        :returns list[reportlab.platypus.Table]: a list of all table rows
             containing the relevant event data
         """
-        return self._elements
+        return self._events
 
-    def get_activities(self):
+    def activities(self):
         """Return the activities shown in the event table
 
-        :return list[str]: a list of all activities
+        :returns list[str]: a list of all activities
         """
         return self._activities
 
-    def get_locations(self):
+    def locations(self):
         """Return the locations at which the events of this table take place
 
-        :return list[str]: a list of all locations
+        :returns list[str]: a list of all locations
         """
         return self._locations
+
+    def title(self):
+        """Return the title of the subtable
+
+        :returns str: the title
+        """
+        return self._title
