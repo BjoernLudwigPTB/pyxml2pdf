050896876d00a945134ebcf37b375f4b550dff6d
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-12-10 22:53:55 +0100

    CHange URL back to permanent

--- pyxml2pdf/main_predefined.py
+++ pyxml2pdf/main_predefined.py
@@ -5,14 +5,14 @@ from pyxml2pdf.core.initializer import Initializer
 def convert():
     input_folder = "input/"
     domain = "https://www.alpinclub-berlin.de/kv/"
-    xml_filename = "kursdaten-draft2020.xml"
+    xml_filename = "kursdaten.xml"
     xml_path = input_folder + xml_filename
     url = domain + xml_filename
     input_path = input_folder + xml_filename
     properties_filename = "kursdaten_prop.properties"
     properties_path = input_folder + properties_filename
     output_folder = "output/"
-    output_filename = "kursdaten-draft2020.pdf"
+    output_filename = "kursdaten.pdf"
     output_path = output_folder + output_filename
     Downloader(url, xml_path)
     Initializer(input_path, output_path, properties_path)
