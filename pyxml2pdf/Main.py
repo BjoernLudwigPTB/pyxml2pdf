710c3f493f00883f211f6f68591c38aead6194e7
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-11-15 17:06:00 +0100

    wip(Main): update execution modules _Main*.py_

--- pyxml2pdf/Main.py
+++ pyxml2pdf/Main.py
@@ -1,38 +1,49 @@
+import os
 import sys
 
+from pyxml2pdf.Core.Downloader import Downloader
 from pyxml2pdf.Core.Initializer import Initializer
 
 
 def main():
-    validate()
-    Initializer(*sys.argv[1:])
+    validate_inputs()
+    if not os.path.isfile(sys.argv[2]):
+        Downloader(*sys.argv[1:3])
+    Initializer(*sys.argv[2:])
     print("\n-------------------------------DONE-------------------------------")
 
 
-def validate():
-    if len(sys.argv) < 3:
+def validate_inputs():
+    if len(sys.argv) < 4:
         raise ValueError(
-            f"We expected three inputs in the commandline parameters, "
+            f"We expected four inputs in the commandline parameters, "
             f"but only {len(sys.argv)} were given. Please specify the "
-            f"URL of the XML file to download or read in, the output "
-            f"PDF's filename and path and the properties file name."
+            f"URL of the XML file to download. The local path to store the "
+            f"downloaded file including the local filename. The output "
+            f"PDF's filename and path and the properties file name and path."
         )
-    if ".xml" not in sys.argv[1]:
+    if "http" not in sys.argv[1] or ".xml" not in sys.argv[1]:
         raise ValueError(
-            f"Expected first commandline parameter to be XML file but "
-            f"{sys.argv[1]} was given. Please specify path and "
+            f"Expected first commandline parameter to be the URL for downloading the "
+            f"XML source file but {sys.argv[1]} was given. Please specify a valid URL "
+            f"including the XML filename."
+        )
+    if ".xml" not in sys.argv[2]:
+        raise ValueError(
+            f"Expected second commandline parameter to be XML file but "
+            f"{sys.argv[2]} was given. Please specify path and "
             f"name of a valid XML file."
         )
-    if ".pdf" not in sys.argv[2]:
+    if ".pdf" not in sys.argv[3]:
         raise ValueError(
-            f"Expected second commandline parameter to be PDF path and filename "
-            f"but {sys.argv[2]} was given. Please specify path and "
+            f"Expected third commandline parameter to be PDF path and filename "
+            f"but {sys.argv[3]} was given. Please specify path and "
             f"valid filename for a PDF file."
         )
-    if ".properties" not in sys.argv[3]:
+    if ".properties" not in sys.argv[4]:
         raise ValueError(
-            f"Expected third commandline parameter to be .properties path and filename "
-            f"but {sys.argv[3]} was given. Please specify path and "
+            f"Expected fourth commandline parameter to be .properties path and "
+            f"filename but {sys.argv[4]} was given. Please specify path and "
             f"valid filename for a .properties file."
         )
 
