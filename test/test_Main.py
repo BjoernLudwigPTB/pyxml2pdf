a6ae499344cc820577a50bff8e75afe0a45438f5
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-09-19 15:14:46 +0200

    refactor(test_Main): remove strange mocking because it does not make sense we believe

--- test/test_Main.py
+++ test/test_Main.py
@@ -1,20 +1,12 @@
 import subprocess
 
 import pytest
-
 from pyxml2pdf import main
 
 
 @pytest.mark.online
 def test_init_main():
-    import mock
-
-    with mock.patch.object(main, "main", return_value=42):
-        with mock.patch.object(main, "__name__", "__main__"):
-            with mock.patch.object(main.sys, "exit") as mock_exit:
-                main.init()
-
-                assert mock_exit.call_args[0][0] == 42
+    main.init()
 
 
 def test_validate_main():
